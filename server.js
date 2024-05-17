99ede015111688e1fb04f4b7fb3f68e84288b69f
Tishalovich Maxim <tishalovichm@gmail.com>
2024-05-17 20:12:21 +0300

    Add tasks adding.

--- server.js
+++ server.js
@@ -3,18 +3,37 @@ import { readFile } from 'node:fs'
 import express from 'express'
 
 const app = express()
+app.use(express.urlencoded({extended: true}))
+
 const port = 8080
 
-app.get('/tasks', (req, res) => {
-    const tasks = [
-        new Task("Task1", "Desc1", TaskStatus.TODO),
-        new Task("Task2", "Desc2", TaskStatus.DOING),
-        new Task("Task3", "Desc3", TaskStatus.DONE)
-    ]
+const tasks = [
+    new Task("Task1", "Desc1", TaskStatus.TODO),
+    new Task("Task2", "Desc2", TaskStatus.DOING),
+    new Task("Task3", "Desc3", TaskStatus.DONE)
+]
 
+app.get('/tasks', (req, res) => {
     res.render('tasks.ejs', {tasks: tasks})
+})
+
+app.get('/add-task-form', (req, res) => {
+    res.render('add-task-form.ejs')
+})
+
+app.post('/tasks', (req, res) => {
+    const task = new Task(
+        req.body.name,
+        req.body.description,
+        req.body.status
+    )
+
+    tasks.push(task)
+
+    res.redirect('/tasks')
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
