d7810f26d4251e5db9ab682cb6fad88f79b7f46a
Tishalovich Maxim <tishalovichm@gmail.com>
2024-06-09 13:51:24 +0300

    Add user dao

---
+++ dal/user_dao.js
@@ -0,0 +1,49 @@
+import pg from 'pg'
+import { User } from '../model/user.js'
+
+class UserDao {
+
+    constructor(pool) {
+        this.pool = pool
+    }
+
+    rowToUser(row) {
+        return new User(
+            row.id,
+            row.login,
+            row.password_hash
+        )
+    }
+
+    async save(user) {
+        const result = await this.pool.query(`
+            INSERT INTO
+                "user"
+                (login, password_hash)
+            VALUES
+                (
+                    '${user.login}',
+                    '${user.passwordHash}'
+                );`
+        )
+    }
+
+    async getById(id) {
+        const queryRes = await this.pool.query(
+            'SELECT * FROM "user" WHERE id = $1',
+            [id]
+        )
+
+        const nRows = queryRes.rows.length
+
+        if (nRows == 0) {
+            return null
+        }
+
+        return this.rowToUser(queryRes.rows[0])
+    }
+
+}
+
+export { UserDao }
+
